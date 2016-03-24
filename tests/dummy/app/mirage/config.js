f23f7e349d39e5be9a83544094cca0b8ffe7abf9
Zach Garwood <zachgarwood@gmail.com>
2016-03-24 16:12:51 -0500

    Added Mirage mock endpoints and traces factory

--- tests/dummy/app/mirage/config.js
+++ tests/dummy/app/mirage/config.js
@@ -1,8 +1,10 @@
 import ENV from '../config/environment';
+import { faker } from 'ember-cli-mirage';
 
 export default function() {
-
   this.urlPrefix = 'https://api.syncano.io';
+
+  /* Endpoints for data objects */
   this.namespace = '/v1/instances/' + ENV.syncano.instance + '/classes';
 
   this.get('/person/objects', function(db) {
@@ -14,19 +16,33 @@ export default function() {
   });
 
   this.post('/person/objects', function(db, request) {
-    console.log(request.requestBody);
-    let thing = db.people.insert(request.requestBody);
-    console.log(thing);
-    return thing;
+    return db.people.insert(request.requestBody);
   });
 
   this.patch('/person/objects/:id', function(db, request) {
-    console.log(request);
     return db.people.update(request.params.id, {});
   });
 
   this.delete('/person/objects/:id', function(db, request) {
     db.people.remove(request.params.id);
     return {};
+  });
+
+  /* Endpoints for codeboxes and traces */
+  this.namespace = '/v1/instances/' + ENV.syncano.instance + '/codeboxes';
+
+  this.post('/:codeboxId/run', function(db, request) {
+    let payload = JSON.parse(request.requestBody).payload;
+    return db.traces.insert({
+      'status': 'processing',
+      'result': { stdout: `${payload.lastName}, ${payload.firstName}` },
+    });
+  });
+
+  this.get('/:codeboxId/traces/:traceId', function(db, request) {
+    if (ENV.environment === 'test' || faker.random.boolean()) {
+      return db.traces.update(request.params.traceId, { 'status': 'success' });
+    }
+    return db.traces.find(request.params.traceId);
   });
 }
