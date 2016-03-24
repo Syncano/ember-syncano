644ec4690f4405e8b91e1918417900372708511d
Zach Garwood <zachgarwood@gmail.com>
2016-03-24 16:36:32 -0500

    Added docblocks to addon code

--- addon/adapters/syncano.js
+++ addon/adapters/syncano.js
@@ -2,7 +2,14 @@ import DS from 'ember-data';
 import Ember from 'ember';
 
 export default DS.Adapter.extend({
+  /**
+   * Uses the default serialize (JSONSerializer).
+   */
   defaultSerializer: '-default',
+
+  /**
+   * Used by the store to retrieve a single record.
+   */
   findRecord(store, type, id) {
     let instance = this.get('syncano.instance');
     return new Ember.RSVP.Promise(function(resolve, reject) {
@@ -15,6 +22,10 @@ export default DS.Adapter.extend({
         });
     });
   },
+
+  /**
+   * Used by the store to create a record.
+   */
   createRecord(store, type, snapshot) {
     let instance = this.get('syncano.instance');
     let record = this.serialize(snapshot, { includeId: true });
@@ -28,6 +39,10 @@ export default DS.Adapter.extend({
         });
     });
   },
+
+  /**
+   * Used by the store to update a record.
+   */
   updateRecord(store, type, snapshot) {
     let instance = this.get('syncano.instance');
     let record = this.serialize(snapshot, { includeId: true });
@@ -41,6 +56,10 @@ export default DS.Adapter.extend({
         });
     });
   },
+
+  /**
+   * Used by the store to delete a record.
+   */
   deleteRecord(store, type, snapshot) {
     let instance = this.get('syncano.instance');
     let record = this.serialize(snapshot, { includeId: true });
@@ -54,6 +73,11 @@ export default DS.Adapter.extend({
         });
     });
   },
+
+  /**
+   * Used by the store to retrieve the entire collection of records of a
+   * certain model.
+   */
   findAll(store, type) {
     let instance = this.get('syncano.instance');
     return new Ember.RSVP.Promise(function(resolve, reject) {
@@ -66,6 +90,12 @@ export default DS.Adapter.extend({
         });
     });
   },
+
+  /**
+   * Used by the store to retrieve a collection of records according to the
+   * filters in `query`. `query` should be formatted exactly as a query obeject
+   * as shown in the Syncano docs.
+   */
   query(store, type, query) {
     let instance = this.get('syncano.instance');
     return new Ember.RSVP.Promise(function(resolve, reject) {
@@ -78,6 +108,12 @@ export default DS.Adapter.extend({
         });
     });
   },
+
+  /**
+   * Used by the store to retrieve a collection that is a subset of records of
+   * based on their ids. It is an optimization for retrieving large sets of
+   * records.
+   */
   findMany(store, type, ids) {
     return this.query(store, type, { 'query': { 'id': { '_in': ids }}});
   },
--- addon/services/syncano.js
+++ addon/services/syncano.js
@@ -1,9 +1,25 @@
 import Ember from 'ember';
 
 export default Ember.Service.extend({
+  /**
+   * Must be set to `true` for services.
+   */
   isServiceFactory: true,
+
+  /**
+   * A Syncano account sub-service, used by `codebox()`.
+   */
   account: null,
+
+  /**
+   * A Syncano instance sub-service, used by the Syncano adapter to interact
+   * with the data store.
+   */
   instance: null,
+
+  /**
+   * Set the `account` and `instance` properties on initialization.
+   */
   init() {
     this.set(
       'account',
@@ -17,6 +33,27 @@ export default Ember.Service.extend({
       })
     );
   },
+
+  /**
+   * Used to run arbitrary code in a Syncano Codebox. It's a thin wrapper
+   * around the `codebox()` method in the Syncano docs, the difference being
+   * that it automatically uses the instance specified in the config
+   * (`ENV.syncano.instance`). Additionally, it has a convenience method
+   * `result()` that runs both `run()` and `trace()` until it gets a successful
+   * result from Syncano.
+   *
+   * Usage:
+   *  syncano: Ember.inject.service(),
+   *  someMethod() {
+   *    let syncano = this.get('syncano');
+   *    let codeboxId = 123;
+   *    let payload = { key_one: 'value one', key_two: 'value_two' };
+   *
+   *    syncano.codebox(codeboxId).result(payload).then(function(result) {
+   *      doSomething(result);
+   *    });
+   *  },
+   */
   codebox(codeboxId) {
     let codebox = this.get('account')
       .instance(this.get('config.instance'))
@@ -25,6 +62,10 @@ export default Ember.Service.extend({
     codebox._getResult = this._getCodeboxResult;
     return codebox;
   },
+
+  /**
+   * A wrapper method around `codebox().run()`.
+   */
   _kickoffCodebox(payload) {
     let codebox = this;
     return new Ember.RSVP.Promise(function(resolve, reject) {
@@ -39,6 +80,12 @@ export default Ember.Service.extend({
         });
     });
   },
+
+  /**
+   * A wrapper method around `codebox().trace()`. If it doesn't get a
+   * `success` response from the `trace()` call, then it schedules
+   * another attempt in 200ms.
+   */
   _getCodeboxResult(id) {
     let codebox = this;
     return new Ember.RSVP.Promise(function(resolve, reject) {
